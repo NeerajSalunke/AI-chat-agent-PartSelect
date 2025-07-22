0af440b4b14a2ffeba2d9f81fe1db0445666e23b
Neeraj Salunke <65599525+NeerajSalunke@users.noreply.github.com>
2025-07-22 16:31:38 -0700

    updated CORS policy

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ai-chat-agent-partselect-1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
